ef2d855e6ebf74e18fda910266244aed0955b731
Kimwonkyu <wonguo84@gmail.com>
2020-12-24 16:07:44 +0900

    2

--- MyProject/src/BOJ_TEST/BOJ_1504_특정최단경로.java
+++ MyProject/src/BOJ_TEST/BOJ_1504_특정최단경로.java
@@ -2,7 +2,7 @@ package BOJ_TEST;
 
 public class BOJ_1504_특정최단경로 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 
 	}
