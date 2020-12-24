5526650548a65109e6852faaa46209c145480c45
Kimwonkyu <wonguo84@gmail.com>
2020-12-24 16:15:24 +0900

    테스

--- MyProject/src/BOJ_TEST/BOJ_1504_특정최단경로.java
+++ MyProject/src/BOJ_TEST/BOJ_1504_특정최단경로.java
@@ -5,6 +5,8 @@ public class BOJ_1504_특정최단경로 {
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
        System.out.println();
+       System.out.println("123");
+       
 	}
 
 }
